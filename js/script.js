764710ab8cfb92b7659005ea582ff34470dc878b
VAleXS <valexs1809@gmail.com>
2023-04-24 22:02:15 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/* alert('Hello'); */
+
+/* const result = confirm('Are you here?');
+console.log(result) */
+
+/* const answer = prompt('Вам есть 18?', '18');
+console.log(answer + typeof(answer));*/
+
+/* const answers = [];
+
+answers[0] = prompt('Какое у Вас имя? ', ' ');
+answers[1] = prompt('Какая у Вас фамилия? ', ' ');
+answers[2] = prompt('Сколько Вам лет? ', ' ');
+
+document.write(answers); */
+
+
+/* const category = 'toys' */
+
+/* console.log('https://someurl.com/' + category) */
+
+/* console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Привет, ${user}`); */
+
+/* console.log(4 + +'8');
+
+let incr = 10,
+    decr = 10; */
+
+/* incr++;
+decr--; */
+
+/* ++incr;
+--decr; */
+
+/* console.log(++incr);
+console.log(--decr);
+
+console.log(5%5); */
+
+/* console.log(2 + 2 * 2 != '6'); */
+
+/* const isChecked = true;
+const isClose = true;
+
+
+console.log(!isChecked || !isClose); */
+
